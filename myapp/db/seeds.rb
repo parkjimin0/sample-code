7feb00f0f0cd598c18496cc5e77692bb5c8988f2
Jimin Park <parkjimin0@gmail.com>
2019-09-18 15:59:26 -0400

    Create a Seed Database

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,7 +1,44 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Beehive.destroy_all
+Cell.destroy_all
+CellType.destroy_all
+
+ActiveRecord::Base.connection.reset_pk_sequence!("beehives")
+ActiveRecord::Base.connection.reset_pk_sequence!("cells")
+ActiveRecord::Base.connection.reset_pk_sequence!("cell_types")
+
+hives_list = [
+    "Jimin's Hive",
+    "Rob's Hive",
+    "Lesley's Hive", 
+    "Whitney's Hive",
+    "Anthony's Hive", 
+]
+
+cell_types_list = [
+    "empty",
+    "honey",
+    "pollen",
+    "egg",
+    "larvae",
+    "pupa"
+]
+
+cell_types_list.each do |name|
+    CellType.create( name: name )
+end
+
+hives_list.each do |name|
+    Beehive.create( name: name )
+end
+
+
+Beehive.all.each do |hive| 
+    hive.cells.create(:name => "Empty Cell", :cell_type_id => 1 )
+    hive.cells.create(:name => "Food", :cell_type_id => 2 )
+    hive.cells.create(:name => "Pollen Room", :cell_type_id => 3 )
+    hive.cells.create(:name => "Egg Bee", :cell_type_id => 4 )
+    hive.cells.create(:name => "Larvae Bee", :cell_type_id => 5 )
+    hive.cells.create(:name => "Baby Bee", :cell_type_id => 6 )
+end
