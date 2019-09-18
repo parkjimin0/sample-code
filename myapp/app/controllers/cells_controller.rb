9de847caa630fea811f9d791a3f24ae2ad836767
Jimin Park <parkjimin0@gmail.com>
2019-09-18 02:56:05 -0400

    Finish Read, Update, Delete with Cells

--- myapp/app/controllers/cells_controller.rb
+++ myapp/app/controllers/cells_controller.rb
@@ -1,7 +1,42 @@
 class CellsController < ApplicationController
+
     def create
         @beehive = Beehive.find(params[:beehive_id])
-        @cell = @beehive.cells.create(params[:cell].permit(:name))
+        @cell = @beehive.cells.create(cell_params)
         redirect_to beehive_path(@beehive)
+    end
+
+    # GET /beehives/1/cells/1
+    def show
+        @beehive = Beehive.find(params[:beehive_id])
+        @cell = @beehive.cells.find(params[:id])
+        render "show"
+    end
+
+    # GET /beehives/1/cells/1/edit
+    def edit
+        @beehive = Beehive.find(params[:beehive_id])
+        @cell = @beehive.cells.find(params[:id])
+        render "edit"
+    end
+
+    def update
+        @beehive = Beehive.find(params[:beehive_id])
+        @cell = @beehive.cells.find(params[:id])
+        @cell.update cell_params 
+        redirect_to beehive_path(@beehive)
+    end
+
+    def destroy
+        @beehive = Beehive.find(params[:beehive_id])
+        @cell = @beehive.cells.find(params[:id])
+        @cell.destroy
+        redirect_to beehive_path(@beehive)
+    end
+
+    private
+
+    def cell_params
+      params.require(:cell).permit!
     end
 end
