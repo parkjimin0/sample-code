0d5ff962d961af3ec2c2d71daac7d073d2260fd7
Jimin Park <parkjimin0@gmail.com>
2019-09-19 16:32:48 -0400

    Add JSON API routes for all cells and individual cell

--- myapp/app/controllers/cells_controller.rb
+++ myapp/app/controllers/cells_controller.rb
@@ -1,7 +1,19 @@
 class CellsController < ApplicationController
-    before_action :set_beehive
+    before_action :set_beehive, :except => [:cells, :cell]
     before_action :set_cell, only: [:show, :edit, :update, :destroy]
-    http_basic_authenticate_with :name => "users", :password => "pass", :except => [:index, :show]
+    http_basic_authenticate_with :name => "users", :password => "pass", :except => [:index, :show, :cells, :cell]
+
+    # GET /cells to see all cells from all hives
+    def cells 
+        @cells = Cell.all
+        render :json => @cells
+    end
+
+    # GET /cell to see any individual cell 
+    def cell
+        @cell = Cell.find(params[:id])
+        render :json => @cell, :include => [:cell_type]
+    end
 
     def create
         @cell = @beehive.cells.create(cell_params.merge(cell_type_id: params[:cell_type_id]))
@@ -13,7 +25,7 @@ class CellsController < ApplicationController
         @cell_types = CellType.all.map{|type| [ type.name, type.id ] }
     end
 
-    # PATCH/PUT /beehives/1/cells/1/edit
+    # PATCH/PUT /beehives/1/cells/1
     def update
         @cell.update cell_params.merge(cell_type_id: params[:cell_type_id])
         redirect_to beehive_path(@beehive)
